e723de9844eb687e8d563db2bc8e19364f6bf5a8
JS0303 <wlstjr0303@gmail.com>
2023-07-19 14:40:37 +0900

    광고문구작성 서비스 구현완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import translate_text_using_chatgpt from "./components/translator";
+import order from "./components/copywriter";
 
 export default function Home() {
+    // 번역 서비스
     const [text, setText] = useState("");
     const [result, setResult] = useState("");
     const [srcLang, setSrcLang] = useState("한국어");
@@ -25,39 +27,88 @@ export default function Home() {
         return;
     };
 
+    // 광고문구 작성 서비스
+    const [product, setProduct] = useState("");
+    const [copy, setCopy] = useState("");
+    const [detail, setDetail] = useState("");
+    const [mood, setMood] = useState("");
+
+    const inputProduct = (event) => setProduct(event.target.value);
+    const inputDetail = (event) => setDetail(event.target.value);
+    const inputMood = (event) => setMood(event.target.value);
+    const onGenerateCopy = async (event) => {
+        event.preventDefault();
+        console.log(product, detail, mood);
+        let slogan = await order(product, detail, mood);
+        if (product === "" || detail === "" || mood === "") {
+            return;
+        }
+        setCopy(slogan);
+        return;
+    };
+
     return (
-        <div className="translator">
-            <title>번역서비스</title>
-            <h1>번역서비스</h1>
-            <br />
-            <form id="translate-form">
-                <input
-                    onChange={inputText}
-                    value={text}
-                    type="text"
-                    placeholder="번역할 텍스트를 입력하세요"
-                ></input>
+        <>
+            <div className="translator">
+                <title>번역서비스</title>
+                <h1>번역서비스</h1>
                 <br />
-                <label>
-                    원본 언어&nbsp;&nbsp;&nbsp;
-                    <select id="src_lang" onChange={selectSrc}>
-                        <option value="한국어">한국어</option>
-                        <option value="영어">영어</option>
-                        <option value="일본어">일본어</option>
-                    </select>
-                </label>
+                <form id="translate-form">
+                    <input
+                        onChange={inputText}
+                        value={text}
+                        type="text"
+                        placeholder="번역할 텍스트를 입력하세요"
+                    ></input>
+                    <br />
+                    <label>
+                        원본 언어&nbsp;&nbsp;&nbsp;
+                        <select id="src_lang" onChange={selectSrc}>
+                            <option value="한국어">한국어</option>
+                            <option value="영어">영어</option>
+                            <option value="일본어">일본어</option>
+                        </select>
+                    </label>
+                    <br />
+                    <label>
+                        목표 언어&nbsp;&nbsp;&nbsp;
+                        <select id="trg_lang" onChange={selectTrg}>
+                            <option value="한국어">한국어</option>
+                            <option value="영어">영어</option>
+                            <option value="일본어">일본어</option>
+                        </select>
+                    </label>
+                </form>
+                <button onClick={onTranslate}>번역</button>
+                <h3>{result}</h3>
+            </div>
+            <div className="copywriter">
+                <h1>광고문구 작성 서비스</h1>
                 <br />
-                <label>
-                    목표 언어&nbsp;&nbsp;&nbsp;
-                    <select id="trg_lang" onChange={selectTrg}>
-                        <option value="한국어">한국어</option>
-                        <option value="영어">영어</option>
-                        <option value="일본어">일본어</option>
-                    </select>
-                </label>
-            </form>
-            <button onClick={onTranslate}>번역</button>
-            <h3>{result}</h3>
-        </div>
+                <form>
+                    {/* 제품 이름: ${product}\n주요 내용: ${detail}\n광고 문구의 스타일: ${mood} */}
+                    <input
+                        onChange={inputProduct}
+                        value={product}
+                        type="text"
+                        placeholder="제품명을 입력하세요"
+                    ></input>
+                    <input
+                        onChange={inputDetail}
+                        value={detail}
+                        type="text"
+                        placeholder="광고할 문구의 주된 내용을 입력하세요"
+                    ></input>
+                    <input
+                        onChange={inputMood}
+                        value={mood}
+                        type="text"
+                        placeholder="광고 문구의 스타일을 입력하세요"
+                    ></input>
+                </form>
+                <button onClick={onGenerateCopy}>광고문구 생성하기</button>
+                <h3>{copy}</h3>
+            </div>
+        </>
     );
 }
---
+++ src/components/copywriter.js
@@ -0,0 +1,35 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    organization: "org-UaLK1XJ9fsXDUO8SYXy9INaf",
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function slogan_generator(prompt) {
+    const system_instruction =
+        "assistant는 마케팅 문구 작성 도우미로 동작한다. user의 내용을 참고하여 마케팅 문구를 작성해라";
+    let messages = [
+        { role: "system", content: system_instruction },
+        { role: "user", content: prompt },
+    ];
+    let model = "gpt-3.5-turbo";
+
+    const response = await openai.createChatCompletion({
+        model: model,
+        messages: messages,
+    });
+
+    const ad_slogan = response.data.choices[0].message.content;
+    console.log("ad_slogan : ", ad_slogan);
+    return ad_slogan;
+}
+
+export default async function order(product, detail, mood) {
+    let prompt = `제품 이름: ${product}\n주요 내용: ${detail}\n광고 문구의 스타일: ${mood} 위 내용을 참고하여 마케팅 문구를 만들어라.`;
+    console.log("prompt : ", prompt);
+    const result = await slogan_generator(prompt);
+    console.log("result : ", result);
+    return result;
+}
